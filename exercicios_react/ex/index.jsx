2a96b23fefe29695149e86a96304af3204ecec01
Lucas <lucaspaulom@hotmail.com>
2018-08-06 09:25:06 -0300

    Trabalhando com mais de um componente.

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Component from './component' // não precisa colocar .jsx pelo fato da configuração resolve do arquivo webpack.config.js
+import Primeiro, { Segundo } from './component'
 
-ReactDOM.render(<Component value='Show!' />, document.getElementById('app')) // propriedade value coloca um valor na props.value
+ReactDOM.render(
+    <div>
+        <Primeiro />
+        <Segundo />
+    </div>
+, document.getElementById('app'))
