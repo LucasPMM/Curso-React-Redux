877e0557b0085aac23239b7b052ca8ab94fd11ab
Lucas <lucaspaulom@hotmail.com>
2018-08-06 17:39:28 -0300

    Passando propriedades para componentes filhos

--- exercicios_react/ex.8/family.jsx
+++ exercicios_react/ex.8/family.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { childrenWithProps } from '../utils/reactUtils'
 
 export default props => (
     <div>
         <h1>Família</h1>
-        { childrenWithProps(props.children, props) }
+        { React.Children.map(props.children,
+            child => React.cloneElement(child, {...props})) }
     </div>
 )
 
--- exercicios_react/webpack.config.js
+++ exercicios_react/webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {      // webpack ira exportar tudo que estiver aqui
             loader: 'babel-loader',     // tipo de loader a ser utilizado
             exclude: /node_modules/,    // arquivos que não devem ser observados pelo loader
             query: {                    // o que o loader vai interpretar (react...)
-                presets: ['es2015', 'react']    // array com todos os presets que queremos aplicar para fazer a tradução -> $ npm i --save-dev babel-core@6.22.1 babel-loader@6.2.10 babel-preset-es2015@6.22.0 ---- $ npm i --save-dev babel-preset-react@6.22.0
+                presets: ['es2015', 'react'],   // array com todos os presets que queremos aplicar para fazer a tradução -> $ npm i --save-dev babel-core@6.22.1 babel-loader@6.2.10 babel-preset-es2015@6.22.0 ---- $ npm i --save-dev babel-preset-react@6.22.0
+                plugins: ['transform-object-rest-spread']    // para funcionar o operador spread (...)
             }
         }]
     }
