ddf5e67fd7cebf548eb5069b63526fe7c6311535
Lucas <lucaspaulom@hotmail.com>
2018-08-05 22:53:54 -0300

    Deixando o componente mais resumido e utilizando o valor da propriedade dos componentes.

--- exercicios_react/ex/component.jsx
+++ exercicios_react/ex/component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default () => ( // transformação de uma função normal em uma função arrow
-    <h1>Primeiro Componente!</h1>
+export default props => ( // transformação de uma função normal em uma função arrow
+    <h1>{props.value}</h1>
 )
--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Component from './component' // não precisa colocar .jsx pelo fato da configuração resolve do arquivo webpack.config.js
 
-ReactDOM.render(<Component />, document.getElementById('app'))
+ReactDOM.render(<Component value='Show!' />, document.getElementById('app')) // propriedade value coloca um valor na props
