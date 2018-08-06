56f73fe86e55a8fb17a732b8e91fc8c243a76043
Lucas <lucaspaulom@hotmail.com>
2018-08-05 22:47:31 -0300

    Transformação de uma função normal em uma função arrow.

--- exercicios_react/ex/component.jsx
+++ exercicios_react/ex/component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
-export default function() {
-    return <h1>Primeiro Componente!</h1>
-}
+export default () => ( // transformação de uma função normal em uma função arrow
+    <h1>Primeiro Componente!</h1>
+)
